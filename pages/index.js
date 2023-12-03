1cdb5fb27c0ce60fab1ce9b814c9a52c95306dc7
anmoln <sirkinggreat@gmail.com>
2023-12-03 21:44:00 +0530

    changes

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Navbar from '../src/components/navbar';
+import Main from '../src/components/Main';
+import Buutton from '../src/components/Buutton';
 export async function getServerSideProps() {
     let initialData = [];
 
@@ -25,40 +27,10 @@ const home = ({ initialData }) => {
         <>
         <Navbar />
         <br></br>
-        <button 
-            className="bg-teal-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-            onClick={fetchData}
-        >
-            Fetch Content
-        </button>
+        <Buutton fetchData={fetchData}/>
         <br></br>
         <br></br>
-            <div className="shadow-lg overflow-hidden border-b border-gray-200 sm:rounded-lg">
-            <table className="min-w-full divide-y divide-gray-200">
-            <thead className="bg-gray-50">
-                <tr>
-                    <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider border">Name</th>
-                    <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider border">Username</th>
-                    <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider border">Email</th>
-                    <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider border">Phone</th>
-                    <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider border">Website</th>
-                    <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider border">Company</th>
-                </tr>
-            </thead>
-                <tbody className="bg-white divide-y divide-gray-200">
-                {fetchContent && data.map((user) => (
-                        <tr key={user.id}>
-                            <td className="px-6 py-4 whitespace-nowrap">{user.name}</td>
-                            <td className="px-6 py-4 whitespace-nowrap">{user.username}</td>
-                            <td className="px-6 py-4 whitespace-nowrap">{user.email}</td>
-                            <td className="px-6 py-4 whitespace-nowrap">{user.phone}</td>
-                            <td className="px-6 py-4 whitespace-nowrap">{user.website}</td>
-                            <td className="px-6 py-4 whitespace-nowrap">{user.company.name}</td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
-        </div></>
+            <Main data={data} fetchContent={fetchContent}/></>
     );
 };
 
